edf60d27562798c00ee1c0ee75bffba0106e6f0c
Holger <holger.trittenbach@gmail.com>
2015-01-28 17:52:03 +0100

    added console prompt to exit main

--- src/de/kit/sem/CarRentalModelApplication.java
+++ src/de/kit/sem/CarRentalModelApplication.java
@@ -1,5 +1,7 @@
 package de.kit.sem;
 
+import java.io.IOException;
+
 public class CarRentalModelApplication {
 	
 	public static int waitingRoomCapacity = 10;
@@ -7,5 +9,13 @@ public class CarRentalModelApplication {
 	
 	public static void main (String[] args){
 		TransitionMatrix transitionMatrix = new TransitionMatrix();
+		System.out.println("## SEM1 Autovermietung ##");
+		
+		try {
+			System.out.println("Push any key to exit");
+	        System.in.read();
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
 	}
 }
